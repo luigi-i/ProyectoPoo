01791bc3cbc698d67dc42b0967b8321e5530b0fc
Luigi <bar24056@uvg.edu.gt>
2024-09-25 21:37:31 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
 
+        String prueba = "hola tonoto";
+        System.out.println(prueba);
 
 
     }
