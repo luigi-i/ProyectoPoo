8536a790b92d06d3791ca0cab28d7bc6a346e852
Ivan <ivanantoniomoratayaortiz@gmail.com>
2024-09-27 23:04:24 -0600

    implementacion de info. de vehiculo en clase vista

--- src/Vista.java
+++ src/Vista.java
@@ -1,4 +1,10 @@
 class Vista {
-
+    public static void mostrarInformacion(Vehiculo Vehiculo,VehiculoDAO vehiculod) {
+        // Aquí solo formateamos y mostramos la información al usuario
+        System.out.println("Información del Vehículo:");
+        System.out.println("Marca: " + Vehiculo.getMarca());
+        System.out.println("Modelo: " + Vehiculo.getModelo());
+        System.out.println("Año: " + Vehiculo.getYear());
+    }
     
 }
